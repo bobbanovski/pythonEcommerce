5d0570a27ede7bb0326e4b079e6f7a0eaf9b7fa2
bobbanovski <bobbanovski@gmail.com>
2017-12-12 23:44:26 +1000

    new app (Module) created

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -30,3 +30,5 @@ urlpatterns = [
 
 if settings.DEBUG: #This is set up in settings.py
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    

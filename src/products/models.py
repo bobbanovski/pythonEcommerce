7e7a754e2539b7c9d6badd360f10645320d3d444
bobbanovski <bobbanovski@gmail.com>
2017-12-15 23:22:20 +1000

    allow title retreival

--- src/products/models.py
+++ src/products/models.py
@@ -9,3 +9,8 @@ class Product(models.Model):
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
 
+    #Needed to return title in admin
+    def __str__(self):
+        return self.title
+    def __unicode__(self):
+        return self.title

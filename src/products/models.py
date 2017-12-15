d5510bd91d6da40f623fa80391200155c9957871
bobbanovski <bobbanovski@gmail.com>
2017-12-15 23:05:13 +1000

    Create Products Model, add to database
    Allow observatikon in admin

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Connect Django to database
+# Save changes with python manage.py makemigrations
+# python manage.py migrate
+class Product(models.Model):
+    title       = models.CharField(max_length=120) #Charfields always have size limits
+    description = models.TextField()
+    price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+

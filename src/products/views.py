f15b182af9576cd83789868a10e9457eb6a97e72
bobbanovski <bobbanovski@gmail.com>
2017-12-15 23:49:10 +1000

    Transfer model contents to view

--- src/products/views.py
+++ src/products/views.py
@@ -9,15 +9,16 @@ class ProductListView(ListView):
     queryset = Product.objects.all() #retrieve from database
     template_name = "products/list.html"
     #view context data - every clas based view has this method - gets the context
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
 
 #Function based view
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
-        'qs': queryset
+        'object_list': queryset
     }
+    print(queryset)
     return render(request, "products/list.html", context)

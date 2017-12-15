0c7d9a40b74431e995618bbb9561c34182029a3d
bobbanovski <bobbanovski@gmail.com>
2017-12-15 23:40:19 +1000

    Views for products model
    Create function and class-based views
    Add to urls.py in main project
    Link to products html file

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,23 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+
+#Class based view - List views
+class ProductListView(ListView):
+    queryset = Product.objects.all() #retrieve from database
+    template_name = "products/list.html"
+    #view context data - every clas based view has this method - gets the context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+#Function based view
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'qs': queryset
+    }
+    return render(request, "products/list.html", context)

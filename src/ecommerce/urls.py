f7ff396416088056dfbc2ecf863a1ffcce05243a
bobbanovski <bobbanovski@gmail.com>
2017-12-16 10:16:30 +1000

    Set up DetailView for Products Model

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -19,16 +19,18 @@ from django.conf.urls import url
 from django.contrib import admin
 from .views import home_page, about_page, contact_page, login_page, register_page
 
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 urlpatterns = [
     url(r'^$', home_page),
-    url(r'^about/', about_page),
-    url(r'^contact/', contact_page),
-    url(r'^login/', login_page),
-    url(r'^register/', register_page),
+    url(r'^about/$', about_page),
+    url(r'^contact/$', contact_page),
+    url(r'^login/$', login_page),
+    url(r'^register/$', register_page),
     url(r'products/$', ProductListView.as_view()),
     url(r'products-fbv/$', product_list_view),
+    url(r'product/(?P<id>\d+)/$', ProductDetailView.as_view()), #id is kwarg
+    url(r'product-fbv/(?P<id>\d+)/$', product_detail_view),
     url(r'^admin/', admin.site.urls),    
 ]
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Product
@@ -22,3 +22,24 @@ def product_list_view(request):
     }
     print(queryset)
     return render(request, "products/list.html", context)
+
+
+#============ Detailed Views ========================
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all() #retrieve from database
+    template_name = "products/detail.html"
+    #view context data - every clas based view has this method - gets the context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+#Function based view
+def product_detail_view(request, id=None, *args, **kwargs):
+    # instance = Product.objects.get(pk=id)
+    instance = get_object_or_404(Product, pk=id)
+    context = {
+        'object': instance
+    }
+    print(instance)
+    return render(request, "products/detail.html", context)

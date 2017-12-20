76b31bc7b9a29b9d2317dac52644f3b335b7b4c1
bobbanovski <bobbanovski@gmail.com>
2017-12-20 21:56:07 +1000

    setup relative urls for product

---
+++ src/products/urls.py
@@ -0,0 +1,23 @@
+#relative urls
+from django.conf.urls import url
+
+
+from products.views import (
+    ProductListView, 
+    # product_list_view, 
+    ProductDetailView, 
+    # product_detail_view, 
+    # ProductFeaturedListView, 
+    # ProductFeaturedDetailView
+)
+
+urlpatterns = [
+    # url(r'^featured/$', ProductFeaturedListView.as_view()), #.as_view needed to prevent error: __init__() takes 1 positional argument but 2
+    # url(r'^featured/(?P<id>\d+)/$', ProductFeaturedDetailView.as_view()),
+    url(r'products/$', ProductListView.as_view()),
+    # url(r'products-fbv/$', product_list_view),
+    url(r'product/(?P<id>\d+)/$', ProductDetailView.as_view()), #id is kwarg
+    # url(r'product-fbv/(?P<id>\d+)/$', product_detail_view),
+]
+
+    
